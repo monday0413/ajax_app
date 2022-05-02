7343078659d90841500a9f0d81ec7d29a04bf7b1
monday0413 <akane.takahashi.3140@gmail.com>
2022-05-02 23:10:27 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,10 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
 
-  # def new
-  # end
-
   def create
     post = Post.create(content: params[:content])
-    redirect_to action: :index  
     render json:{ post: post }
   end
 end
